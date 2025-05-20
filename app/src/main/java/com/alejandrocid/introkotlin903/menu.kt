e1f1421da3279a8fb5b86f4b3364659138916037
unknown <79204@alumnos.utleon.edu.mx>
2025-05-19 19:50:39 -0600

    OperasBas

---
+++ app/src/main/java/com/alejandrocid/introkotlin903/menu.kt
@@ -0,0 +1,41 @@
+package com.alejandrocid.introkotlin903
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.alejandrocid.introkotlin903.practica1.saludoActivity
+import com.alejandrocid.introkotlin903.practica2.OperasActivity
+
+class menu : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_menu)
+
+        val btnSaludo = findViewById<Button>(R.id.btn1)
+        btnSaludo.setOnClickListener{navegateToSaludo()}
+
+        val btnOperas = findViewById<Button>(R.id.btn2)
+        btnOperas.setOnClickListener{navegateToOperas()}
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    private fun navegateToSaludo(){
+        val intent = Intent(this, saludoActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun navegateToOperas(){
+        val intent = Intent(this, OperasActivity::class.java)
+        startActivity(intent)
+    }
+}
--- app/src/main/java/com/alejandrocid/introkotlin903/temasKotlin/IaNarrow.kt
+++ app/src/main/java/com/alejandrocid/introkotlin903/temasKotlin/IaNarrow.kt
@@ -1,8 +1,8 @@
 package com.alejandrocid.introkotlin903.temasKotlin
 
 fun clasificarSentimiento(frase: String): String {
-    val palabrasPositivas = listOf("bueno", "genial", "excelente", "feliz", "increíble", "me gusta")
-    val palabrasNegativas = listOf("malo", "horrible", "terrible", "triste", "pésimo", "no me gustame ")
+    val palabrasPositivas = listOf("bueno", "genial", "excelente", "feliz", "increíble", "me gusta", "éxito", "felicidad", "increíble", "maravilloso", "amor", "excelente", "esperanza", "alegría", "triunfo", "brillante")
+    val palabrasNegativas = listOf("aburrida", "malo", "horrible", "terrible", "triste", "pésimo", "no me gusta", "fracaso", "tristeza", "terrible", "horrible", "odio", "pésimo", "desesperanza", "dolor", "derrota", "oscuro")
 
     val palabras = frase.lowercase().split(" ")
 
